1bcc8ec514bdb8b1b8f264b40ee803c268aa0e89
Hanzhi Yin <yinhanzhi@gmail.com>
2020-07-11 20:28:15 -0500

    whole proj lib migrate to local JSON

--- project/script.js
+++ project/script.js
@@ -1,12 +1,124 @@
+'use strict';
+// ================================================================
+// Constant Declaration
+// ================================================================
+
 const codeList = document.getElementById('code-list');
 const compList = document.getElementById('comp-list');
 const codeBtn = document.getElementById('code-btn');
 const compBtn = document.getElementById('comp-btn');
+
+const monthAbbr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 const toggle = () => {
     codeList.classList.toggle('d-none');
     codeBtn.classList.toggle('active');
     compList.classList.toggle('d-none');
     compBtn.classList.toggle('active');
 }
+
+const newPara = (c, ...classList) => {
+    const p = document.createElement('p');
+    p.classList.add(...classList)
+    p.innerText = c;
+    return p;
+}
+
+const newSpan = (c, ...classList) => {
+    const s = document.createElement('span');
+    s.classList.add(...classList);
+    s.innerText = c;
+    return s;
+}
+
+const newLink = (c, l, n, ...classList) => {
+    const a = document.createElement('a');
+    a.classList.add(...classList);
+    a.innerText = c;
+    a.setAttribute('href', l);
+    if (n)
+        a.setAttribute('target', '_blank');
+    return a;
+}
+
+const newDiv = (...classList) => {
+    const d = document.createElement('div');
+    d.classList.add(...classList);
+    return d;
+}
+
+const genComp = (d) => {
+    const { id, isMain, title, year, month, desc, shortDesc, links, tags, imgs } = d;
+
+    const cardHeader = newDiv('card-header');
+    cardHeader.append(newPara(title, 'font-weight-bold', 'h4'), newPara(shortDesc));
+        const cardBtnList = newDiv('d-flex', 'justify-content-start', 'align-items-center');
+        cardBtnList.append(newSpan(`${monthAbbr[month - 1]} ${year}`, 'badge', 'badge-pill', 'badge-success'));
+        if (isMain)
+            cardBtnList.append(newSpan('Major Work', 'badge', 'badge-pill', 'badge-warning'));
+        if (tags.length !== 0)
+            for (const tag of tags)
+                cardBtnList.append(newSpan(tag, 'badge', 'badge-pill', 'badge-warning', 'mx-1'));
+        cardBtnList.append(newSpan('', 'm-auto'));
+        const cardInfoBtn = document.createElement('button');
+            cardInfoBtn.classList.add('btn', 'btn-info', 'btn-sm');
+            cardInfoBtn.setAttribute('data-toggle', 'collapse');
+            cardInfoBtn.setAttribute('data-target', `#${id}`);
+            cardInfoBtn.innerText = 'More Info';
+        cardBtnList.append(cardInfoBtn);
+    cardHeader.append(cardBtnList);
+
+    const cardMain = newDiv('collapse');
+    cardMain.id = id;
+    cardMain.setAttribute('data-parent', '#comp-list');
+        const cardBody = newDiv('card-body');
+            const cardLinkList = newDiv('d-flex', 'justify-content-start', 'flex-wrap')
+            for (const prop in links) {
+                cardLinkList.append(newLink(prop, links[prop], true, 'btn', 'btn-link'));
+            }
+        cardBody.append(newPara(desc), cardLinkList);
+    cardMain.append(cardBody);
+
+    const card = newDiv('card', 'm-3');
+    card.append(cardHeader, cardMain);
+    return card;
+}
+
+const genCode = (d) => {
+    const { id, isMain, title, year, month, desc, shortDesc, links, tags, imgs } = d;
+}
+
+const sortData = (f, s) => {
+    if (f.isMain - s.isMain !== 0) return s.isMain - f.isMain;
+    if (f.year - s.year !== 0) return f.year - s.year;
+    return f.month - s.month;
+}
+
+// ================================================================
+// Async procedures
+// ================================================================
+
+const fetchComp = async () => {
+    try {
+        const data = JSON.parse(await (await fetch('./lib/comp.json')).text());
+        data.sort(sortData);
+        data.forEach(d => compList.append(genComp(d))); 
+    } catch (e) { console.error(e); }
+}
+
+const fetchCode = async () => {
+    try {
+        const data = JSON.parse(await (await fetch('./lib/code.json')).text());
+        data.sort(sortData);
+        data.forEach(d => codeList.append(genCode(d))); 
+    } catch (e) { console.error(e); }
+}
+
+// ================================================================
+// Main
+// ================================================================
+
+fetchComp();
+fetchCode();
 codeBtn.onclick = _ => { if (!codeBtn.classList.contains('active')) toggle() }
 compBtn.onclick = _ => { if (!compBtn.classList.contains('active')) toggle() }
